fee0106e94c9b199a81259c2c8a0db4699e23c91
Peter Yoon <jonghyeokyoon19@gmail.com>
2025-04-24 00:04:57 -0700

    minor tweaks

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,9 +9,12 @@ const Hero = () => {
     <div className="flex h-[90vh] items-center bg-[linear-gradient(135deg,_#F5F9FF_0%,_#fff_100%)] relative overflow-hidden">
         <div className="container mx-auto px-30">
             <div className="hero-content w-[60%]">
-            <h1 className="text-5xl font-bold text-[#222222] mb-6">Software @ SAP</h1>
-            <p className="text-xl text-[#4A90E2] mb-8">Passionate about solving problems using data</p>
-            <p className="pb-8">Hi! My name is Peter, and I'm currently a Software Engineer Intern at SAP.</p>
+            <h1 className="text-5xl font-bold text-[#222222] mb-6">I'm a Software Engineer</h1>
+            <p className="text-xl text-[#4A90E2] mb-8">Software Engineer Intern @ SAP</p>
+            <p className="pb-8">Hi! I'm Peter, a senior at UC San Diego studying Mathematics and Computer Science.<br></br>
+          I'm currently a Data Engineering Intern at SAP, and I've previously interned as a Software Engineer at RapidSOS and UCSD IT Services.<br></br>
+          I'm always passionate about learning something new to solve problems with software!
+            </p>
             <a href="#projects" className="cta-button inline-block bg-[#2C5282] text-white py-3 px-8 rounded-md font-semibold transition-colors duration-300 hover:bg-[#1A365D]">
                 Currently WIP!
             </a>
